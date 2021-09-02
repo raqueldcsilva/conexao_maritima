d226bdb26b4229d62ec55cc80d1aa56213ff73c7
Jessica <jehssicamiyuki@gmail.com>
2021-09-02 11:41:42 -0300

    task6 - crud da postagem e model do usuario atulizado 02.09

--- conexaomaritima/src/main/java/br/com/generation/model/Postagem.java
+++ conexaomaritima/src/main/java/br/com/generation/model/Postagem.java
@@ -42,6 +42,18 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
 	public long getId() {
 		return id;
--- conexaomaritima/src/main/java/br/com/generation/model/Usuario.java
+++ conexaomaritima/src/main/java/br/com/generation/model/Usuario.java
@@ -1,13 +1,19 @@
 package br.com.generation.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_usuarios")
@@ -29,8 +35,20 @@ public class Usuario {
 	
 	@NotNull
 	@NotBlank(message = "Insira sua senha")
-	@Size(min = 10, max = 12, message = "A senha deve ter no mínimo 10 e no máximo 12 caracteres")
+	@Size(min = 8, message = "A senha deve ter no mínimo 10 e no máximo 12 caracteres")
 	private String senha;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
 
 	public long getId() {
 		return id;
